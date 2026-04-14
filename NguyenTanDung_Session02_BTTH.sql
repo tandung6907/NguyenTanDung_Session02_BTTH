1f53c473a09cab7ea6fd20088e29a8bc13bfad07
tandung6907 <ntdung692007@gmail.com>
2026-04-14 15:46:49 +0700

    Update book and borrow_books table definitions

--- NguyenTanDung_Session02_BTTH.sql
+++ NguyenTanDung_Session02_BTTH.sql
@@ -3,13 +3,13 @@ use homework;
 create table book(
 	book_id char(5) primary key,
     book_name varchar(200) not null,
-    quantity int check (quantity > 0),
+    quantity int not null check (quantity > 0),
     rent decimal(10,2) default 5000
 );
 
 create table borrow_books(
 	borrow_id int auto_increment primary key,
-    book_id char(5),
+    book_id char(5) not null,
     foreign key (book_id) references book(book_id),
     borrow_date timestamp default (current_timestamp)
 );
